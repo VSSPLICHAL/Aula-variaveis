bdbf48551e4544a579d4c33f87dad39b801a12b9
VSSPLICHALl <splichalvictor@gmail.com>
2024-02-04 14:10:21 -0300

    uso da aula

---
+++ variaveis-const.js
@@ -0,0 +1,11 @@
+// texto igual a string
+//const. nao pode ser alterado, somente let. pois é de repetição. (const ou let)
+let primeiroNome = "Victor";
+
+
+console.log(primeiroNome);
+
+primeiroNome = "Indy"
+
+console.log(primeiroNome)
+
